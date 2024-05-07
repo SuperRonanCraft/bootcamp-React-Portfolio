03a91cd88d68f807d1bad6859827b7d29d73f7d0
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-06 21:41:52 -0400

    dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import env from 'react-dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: env.PORT || 3001,
     open: true,
   },
   resolve: {
