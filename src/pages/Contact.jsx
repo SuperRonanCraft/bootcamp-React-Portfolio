ee362602df8651a11f803b525cf4f9dc76ffdfd4
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-07 00:44:29 -0400

    form schema

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,83 @@
-function AboutPage() {
-  return <div></div>;
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormLabel,
+} from '../components/ui/form';
+import { Input } from '../components/ui/input';
+import { Button } from '../components/ui/button';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: 'Name must be atleast 2 characters long.',
+  }),
+  email: z.string().email({
+    message: 'Please provide a valid email address.',
+  }),
+});
+
+function Contact() {
+  //Form Schema
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      first: '',
+      last: '',
+    },
+  });
+
+  //Form Submit Event
+  function onSubmit(values) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <>
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormDescription>Please provide your full name</FormDescription>
+                <FormMessage />
+              </FormItem>
+            </>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <>
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="joe@email.com" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Please provide your professional email
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            </>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
 
-export default AboutPage;
+export default Contact;
