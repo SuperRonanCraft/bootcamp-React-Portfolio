3a860a480bfbe687a79371889596788fcb3cf718
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-08 00:33:46 -0400

    about section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header({ darkmode, darkModeHandler }) {
     <div className="grid grid-cols-3 gap-4 px-4 pt-3 pb-2 content-center bg-gray-200 dark:bg-gray-800">
       <Navigation />
       <h1 className="flex justify-center items-center text-nowrap text-center text-lg md:text-2xl font-semibold">
-        Alains React Portfolio
+        My Portfolio
       </h1>
       <button className="justify-self-end" onClick={() => darkModeHandler()}>
         {darkmode && <Moon size={32} />}
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,6 +5,38 @@ function AboutPage() {
   console.log(projects);
   return (
     <div>
+      {/* About */}
+      <div className="mx-5">
+        <div className="max-w-screen-lg mx-auto mt-5 bg-gray-100 dark:bg-gray-700 shadow-md rounded-lg overflow-hidden">
+          <div className="px-6 py-4">
+            <h2 className="text-2xl font-bold mb-2 ">Hello, I'm Alain</h2>
+            <p className="text-base">
+              A dedicated software engineer-in-training with a flair for
+              crafting innovative web applications and game modifications.
+              Currently enrolled in a rigorous software engineering bootcamp,
+              I'm honing my skills in various technologies while exploring the
+              realms of web development and game design.
+            </p>
+            <p className="text-base mt-2">
+              With a diverse portfolio ranging from intricate mods for games to
+              dynamic web applications like payment systems using Stripe and
+              telephony solutions, I thrive on the challenge of solving
+              real-world problems through code. Recent ventures into React
+              applications showcasing a commitment to staying at the forefront
+              of industry trends.
+            </p>
+            <p className="text-base mt-2">
+              I'm passionate about seeing my creations come to life in
+              production environments, finding fulfillment in the impact of my
+              work as it becomes part of users' everyday experiences. With a
+              keen eye for detail and a relentless drive for innovation, I'm
+              poised to make waves in the ever-evolving landscape of software
+              development.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Projects */}
       <section>
         <h2 className="px-3 pt-5 font-bold">Recent Projects</h2>
         <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 px-4 py-4">
@@ -14,6 +46,7 @@ function AboutPage() {
         </div>
       </section>
 
+      {/* Skill Icons */}
       <section>
         <h2 className="px-3 pt-5 font-bold">Skills</h2>
         <Skills />
