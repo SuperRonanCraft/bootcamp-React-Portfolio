11009d42b8e834d479fa49f32c8d8a9885dfdaa9
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-10 21:40:37 -0400

    resume iFrame

---
+++ src/components/ui/frameWrapper.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const FrameWrapper = ({ html }) => {
+  return (
+    <div className="my-2">
+      <iframe
+        className="mx-auto w-[800px] overflow-x-scroll h-[210vh]"
+        src={html}
+        // seamless="seamless"
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
+};
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,14 @@
+import { FrameWrapper } from '../components/ui/frameWrapper';
+
 function AboutPage() {
-  return <></>;
+  const resumeLink =
+    'https://docs.google.com/document/d/e/2PACX-1vS71dChpXsHghcLFp8WFo1eW7ypMYzmYmYmj0YN0lji_KY-CHItRViOAlvuATBjY1dWKSxNTXpuCIZ4/pub?embedded=true';
+  return (
+    <div className="">
+      {/* Google Docs iFrame :D */}
+      <FrameWrapper html={resumeLink} />
+    </div>
+  );
 }
 
 export default AboutPage;
